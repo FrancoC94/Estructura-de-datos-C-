b27affbfefb302c7fe1067b2beaac4b73739c477
FrancoC94 <cj.francom@uea.edu.ec>
2025-07-06 06:26:59 -0500

    Update build outputs and add SourceLink support
    TAREA SEMANA 6

--- csharpProjects/TareaSemana6/obj/Debug/net9.0/TareaSemana6.AssemblyInfo.cs
+++ csharpProjects/TareaSemana6/obj/Debug/net9.0/TareaSemana6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareaSemana6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3a3219e173eb471c6f20275354d8c05edb92a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareaSemana6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareaSemana6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
