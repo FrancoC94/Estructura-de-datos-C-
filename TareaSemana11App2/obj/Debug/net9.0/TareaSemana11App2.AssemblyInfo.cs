fdbabdbabfc455adb03129fe9c34026e0695f964
FrancoC94 <cj.francom@uea.edu.ec>
2025-08-31 10:46:38 -0500

    TareaSemana11App2

--- TareaSemana11App2/obj/Debug/net9.0/TareaSemana11App2.AssemblyInfo.cs
+++ TareaSemana11App2/obj/Debug/net9.0/TareaSemana11App2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareaSemana11App2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7584e56bafebad8e61f6c062e51ec54c796a8693")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28443638f4230e034828afe41541b2dcc44b829c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareaSemana11App2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareaSemana11App2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
