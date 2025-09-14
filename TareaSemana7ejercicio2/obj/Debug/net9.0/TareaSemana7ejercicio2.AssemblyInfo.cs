78dba5e80058cbef939eefd7b59754ca0a9761ab
FrancoC94 <cj.francom@uea.edu.ec>
2025-09-14 11:27:37 -0500

    TareaSemana7ejercicio2

--- TareaSemana7ejercicio2/obj/Debug/net9.0/TareaSemana7ejercicio2.AssemblyInfo.cs
+++ TareaSemana7ejercicio2/obj/Debug/net9.0/TareaSemana7ejercicio2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareaSemana7ejercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75e8f39c03d3516deb9b1185f7fce3478e4a5cf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b5469836a71d95ea09f03e8a2705fa41d00df0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareaSemana7ejercicio2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareaSemana7ejercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
